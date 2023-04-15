120c01136d04644c1d897ad62a10a034e5d7fd12
Oleksii Hiriavenko <martmarchmartmarch@gmail.com>
2023-04-16 02:34:05 +0300

    improove filter logic

--- src/js/modules/filter.js
+++ src/js/modules/filter.js
@@ -5,9 +5,16 @@ const searchInput = document.querySelector("#searchInput");
 const selectStatus = document.querySelector("#selectStatus");
 const selectPriority = document.querySelector("#selectPriority");
 
-searchBtn.addEventListener("click", () => {
-    const cardsArr = Array.from(document.querySelectorAll(".card"));
+const filterParams = [selectStatus, selectPriority];
 
+filterParams.forEach((param) => {
+    param.addEventListener("change", filter);
+});
+
+searchInput.addEventListener("input", filter);
+
+function filter() {
+    const cardsArr = Array.from(document.querySelectorAll(".card"));
     cardsArr.forEach((card) => {
         const purposeProperty = card.querySelector(".purpose");
         card.parentNode.classList.add("d-none");
@@ -29,7 +36,33 @@ searchBtn.addEventListener("click", () => {
             card.parentNode.classList.replace("d-none", "d-block");
         }
     });
-});
+}
+
+// searchBtn.addEventListener("click", () => {
+//     const cardsArr = Array.from(document.querySelectorAll(".card"));
+
+//     cardsArr.forEach((card) => {
+//         const purposeProperty = card.querySelector(".purpose");
+//         card.parentNode.classList.add("d-none");
+
+//         if (
+//             selectStatus.value === "" &&
+//             selectPriority.value === "" &&
+//             searchInput.value.trim() === ""
+//         ) {
+//             card.parentNode.classList.replace("d-none", "d-block");
+//         }
+
+//         if (
+//             (card.dataset.status === selectStatus.value || selectStatus.value === "") &&
+//             (card.dataset.priority === selectPriority.value || selectPriority.value === "") &&
+//             (purposeProperty.innerText.toLowerCase().includes(searchInput.value.toLowerCase()) ||
+//                 searchInput.value.trim() === "")
+//         ) {
+//             card.parentNode.classList.replace("d-none", "d-block");
+//         }
+//     });
+// });
 
 resetBtn.addEventListener("click", () => {
     const cardsArr = Array.from(document.querySelectorAll(".card"));
